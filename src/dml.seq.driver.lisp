05992cc447ea0eff457c569677e0b8a0dff37c78
cuichao <cuic@chinare.com.cn>
2018-03-14 16:53:00 +0800

    working on cario

--- src/dml.seq.driver.lisp
+++ src/dml.seq.driver.lisp
@@ -1,14 +1,26 @@
 
 (in-package :dml.seq)
 
+;;参数
+(defconstant MIN-X-MARGIN 12.0)
+(defconstant MIN-Y-MARGIN 24.0)
+
+;;
+(defun ignore-warning (condition)
+  (declare (ignore condition))
+  (muffle-warning))
+(defmacro igw (&rest forms)
+  `(handler-bind ((warning #'ignore-warning))
+     ,@forms))
+
 ;;不变量形式存在的对象范围
 (defclass dml-extents ()
-  ((x-bearing :reader x-bearing :initarg :x-bearing :initform 0)
-   (y-bearing :reader y-bearing :initarg :y-bearing :initform 0)
-   (label-end :reader width :initarg :width :initform 0)
-   (height :reader height :initarg :width :initform 0)))
+  ((x-bearing :reader x-bearing :initarg :x-bearing :initform 0.0)
+   (y-bearing :reader y-bearing :initarg :y-bearing :initform 0.0)
+   (label-end :reader width :initarg :width :initform 0.0)
+   (height :reader height :initarg :height :initform 0.0)))
 
-(defgeneric get-dml-extents(element)
+(defgeneric get-dml-extents (element)
   (:documentation "get dml extents in current contex"))
 
 (defgeneric draw-dml-element (element x y)
@@ -16,7 +28,22 @@
 
 ;;从调用消息起点开始绘制消息
 (defmethod get-dml-extents ((msg call-message))
-  ())
+  (let ((label-ext (igw (get-text-extents (label call-message)))))
+    (make-instance 'dml-extents :x-bearing 0.0
+                                :y-bearing (* -1 MIN-Y-MARGIN)
+                                :width (+ (* 2 MIN-X-MARGIN) (text-width label-ext))
+                                :height (+ (* 2 MIN-Y-MARGIN) (text-height label-ext)))))
+;;以左下角为起点绘制文本
+(defun draw-text-on-start-point(text x y)
+  (let (ext (igw (get-text-extents text)))
+    (move-to (- x (text-x-bearing ext))
+             (- y (text-y-bearing ext)))
+    (show-text text)))
+
+;;绘制直线箭头
+(defun draw-arrow from-x from-y to-x to-y :style)
+
+
 (defmethod draw-dml-element ((msg call-message) x y)
   ())
 
@@ -66,3 +93,24 @@
 
 ;; extend组合规则
 
+;;准备好绘制环境
+
+(defparameter *context*
+  (create-ps-context
+   (concatenate 'string
+                (uiop:native-namestring
+                 (uiop:temporary-directory))
+                "Temp.DML.ps")
+   200 200))
+
+;;测试绘制函数
+(with-png-file ("/tmp/test.png" :rgb24 200 200)
+  (set-source-rgb 0.2 0.2 1)
+  (paint)
+  (translate 100 100)
+  (rotate 1.15)
+  (move-to 0 0)
+  (line-to 60 0)
+  (set-source-rgb 1 1 1)
+  (set-line-width 5)
+  (stroke))
